a418a0c55b703d1c98a0adb6e243341edcf6908f
Ryota Sugimoto <ryota.sugimoto@gmail.com>
2018-01-30 11:18:41 +0900

    reconst loss was wrong

--- main.py
+++ main.py
@@ -4,23 +4,37 @@ import numpy as np
 import time
 from model import KmerAutoencoder
 
+'''
 def data_generator(f):
-  f.seek(0)
   def gen():
-    for s in f:
-      r1 = np.array([float(n) for n in s.strip().split()[1:]],
-                    dtype=np.float32)
-      r2 = np.array([float(n) for n in next(f).strip().split()[1:]],
-                    dtype=np.float32)
-      yield (r1+r2)/np.sum(r1+r2)
+    while True:
+      f.seek(0)
+      for s in f:
+        r1 = np.array([float(n) for n in s.strip().split()[1:]],
+                      dtype=np.float32)
+        r2 = np.array([float(n) for n in next(f).strip().split()[1:]],
+                      dtype=np.float32)
+        yield (r1+r2)/np.sum(r1+r2)
   return gen
+'''
+def data_generator(f):
+  l = []
+  for s in f:
+    l.append(np.array([float(n) for n in s.strip().split()[2:]],
+                      dtype=np.float32))
+  def gen():
+    while True:
+      for mer in l:
+        yield mer
+  return gen
+
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('train_kmer_file', type=argparse.FileType('r'))
 parser.add_argument('test_kmer_file', type=argparse.FileType('r'))
-parser.add_argument('test_codes', type=argparse.FileType('w'))
-parser.add_argument('--batch_size', type=int, default=1000)
+parser.add_argument('test_codes_out', type=str)
+parser.add_argument('--batch_size', type=int, default=100)
 args = parser.parse_args()
 
 dataset = tf.data.Dataset.from_generator(data_generator(args.train_kmer_file),
@@ -32,19 +46,23 @@ batch_iterator = batch.make_one_shot_iterator()
 test_kmer = []
 test_read_name = []
 test_taxon = []
+args.test_kmer_file.seek(0)
 for s in args.test_kmer_file:
   l = s.strip().split()
   test_read_name.append(l[0])
   test_taxon.append(l[1])
-  r1 = np.array([ float(n) for n in l[3:]], dtype=np.float32)
+  r1 = np.array([ float(n) for n in l[2:]], dtype=np.float32)
+  test_kmer.append(r1)
+  '''
   r2 = np.array([ float(n) 
-                  for n in next(args.test_kmer_file).strip().split()[3:]],
+                  for n in next(args.test_kmer_file).strip().split()[2:]],
                 dtype=np.float32)
   test_kmer.append((r1+r2)/np.sum(r1+r2))
+  '''
 test_kmer = np.array(test_kmer)
 test_size = len(test_read_name)
 
-KA = KmerAutoencoder(num_neurons=500, num_codes=40)
+KA = KmerAutoencoder(num_neurons=500, num_hidden_layers=8, num_codes=10)
 ll,rl,opt = KA.train(batch_iterator.get_next(), learning_rate=0.001)
 
 _,_,test_codes,_,_,test_ll,test_rl = KA.model(test_kmer)
@@ -54,21 +72,23 @@ num_batch = 0
 with tf.Session() as sess:
   init.run()
   begin = time.clock()
-  while True:
+  while num_batch < 10000:
     try:
       latent_loss, reconst_loss, _ = sess.run([ll,rl,opt])
     except tf.errors.OutOfRangeError:
       break
     num_batch += 1
     if num_batch % 100 == 0:
-      print('loss', latent_loss/args.batch_size,
+      print('train_loss', latent_loss/args.batch_size,
                     reconst_loss/args.batch_size,
                     (latent_loss+reconst_loss)/args.batch_size, flush=True)
       print('batch', num_batch, time.clock() - begin, flush=True)
       begin = time.clock()
-  tes_c,tes_ll,tes_rl = sess.run([test_codes, test_ll, test_rl])
-  codes = [ list(c) for c in tes_c ]
-  print(tes_ll/test_size,tes_rl/test_size)
-  for name, taxon, c in zip(test_read_name, test_taxon, codes):
-    print('\t'.join([name, taxon] + list(map(str,c))),file=args.test_codes)
-  args.test_codes.close()
+      
+      tes_c,tes_ll,tes_rl = sess.run([test_codes, test_ll, test_rl])
+      codes = [ list(c) for c in tes_c ]
+      print('test_loss', tes_ll/test_size, tes_rl/test_size)
+      code_file = open(args.test_codes_out+'_'+str(num_batch), 'w')
+      for name, taxon, c in zip(test_read_name, test_taxon, codes):
+        print('\t'.join([name, taxon] + list(map(str,c))),file=code_file)
+      code_file.close()
--- model.py
+++ model.py
@@ -1,24 +1,28 @@
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 from tensorflow.contrib.layers import batch_norm
+from math import pi
 
 class KmerAutoencoder:
-  def __init__(self, num_neurons, num_codes):
+  def __init__(self, num_neurons, num_hidden_layers, num_codes):
     self.num_neurons = num_neurons
+    self.num_hidden_layers = num_hidden_layers
     self.num_codes = num_codes
   
   def model(self, X):
     def res_full(input, n_out, scope):
       out = fully_connected(input, n_out,
+                            activation_fn=tf.tanh,
                             normalizer_fn=batch_norm,
                             reuse=tf.AUTO_REUSE, scope=scope)
       return tf.add(input, out)
     
     encoder_out = fully_connected(X, self.num_neurons,
+                                  activation_fn=tf.tanh,
                                   normalizer_fn=batch_norm,
                                   reuse=tf.AUTO_REUSE,
                                   scope='encoder1')
-    for n_layer in range(2,9):
+    for n_layer in range(2, self.num_hidden_layers+1):
       encoder_out = res_full(encoder_out, 
                              self.num_neurons, scope='encoder'+str(n_layer))
     
@@ -33,23 +37,31 @@ class KmerAutoencoder:
     codes = mean + sigma * noise
     
     decoder_out = fully_connected(codes, self.num_neurons,
+                                  activation_fn=tf.tanh,
                                   normalizer_fn=batch_norm,
                                   reuse=tf.AUTO_REUSE,
                                   scope='decoder1')
-    for n_layer in range(2,9):
+    for n_layer in range(2, self.num_hidden_layers+1):
       decoder_out = res_full(decoder_out,
                              self.num_neurons, scope='decoder'+str(n_layer))
 
     logits = fully_connected(decoder_out, 136,
                              activation_fn=None,
                              reuse=tf.AUTO_REUSE, scope='logits')
-    outputs = tf.nn.sigmoid(logits)
+    o_mean = tf.nn.sigmoid(logits)
+    o_gamma = fully_connected(decoder_out, 136,
+                              activation_fn=None,
+                              reuse=tf.AUTO_REUSE, scope='o_gamma')
+    o_sigma = tf.exp(0.5 * o_gamma)
+    
 
     latent_loss = 0.5 * tf.reduce_sum(
       tf.exp(gamma) + tf.square(mean) - 1 - gamma)
+
     reconstruction_loss = tf.reduce_sum(
-      tf.nn.sigmoid_cross_entropy_with_logits(labels=X, logits=logits))
-    return mean,gamma,codes,logits,outputs,latent_loss,reconstruction_loss
+      0.5*tf.log(2*pi) + 0.5*o_gamma \
+      + tf.losses.mean_squared_error(labels=X, predictions=o_mean)/(2*o_sigma**2))
+    return mean,gamma,codes,logits,o_mean,latent_loss,reconstruction_loss
 
   def train(self, X, learning_rate):
     mean,gamma,codes,logits,outputs,ll,rl = self.model(X)
